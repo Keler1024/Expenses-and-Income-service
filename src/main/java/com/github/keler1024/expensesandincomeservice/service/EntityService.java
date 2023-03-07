ab1970385934c0a94889e9abc43423cf48629a72
Keler1024 <sergeydrozdov.1999@gmail.com>
2023-03-07 18:02:10 +0300

    implementation of update and add service methods moved to EntityService to decrease code duplication.

--- src/main/java/com/github/keler1024/expensesandincomeservice/service/EntityService.java
+++ src/main/java/com/github/keler1024/expensesandincomeservice/service/EntityService.java
@@ -1,16 +1,17 @@
 package com.github.keler1024.expensesandincomeservice.service;
 
+import com.github.keler1024.expensesandincomeservice.data.entity.Change;
 import com.github.keler1024.expensesandincomeservice.exception.ResourceNotFoundException;
 import com.github.keler1024.expensesandincomeservice.exception.UnauthorizedAccessException;
 import com.github.keler1024.expensesandincomeservice.model.converter.IRequestToEntityToResponseConverter;
 import com.github.keler1024.expensesandincomeservice.security.AuthContext;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public abstract class BaseService<Q, E, S, R extends JpaRepository<E, Long>> {
+public abstract class EntityService<Q, E, S, R extends JpaRepository<E, Long>> {
     protected final R entityRepository;
     protected final IRequestToEntityToResponseConverter<Q, E, S> converter;
 
-    public BaseService(R entityRepository, IRequestToEntityToResponseConverter<Q, E, S> converter) {
+    public EntityService(R entityRepository, IRequestToEntityToResponseConverter<Q, E, S> converter) {
         this.entityRepository = entityRepository;
         this.converter = converter;
     }
@@ -22,10 +23,7 @@ public abstract class BaseService<Q, E, S, R extends JpaRepository<E, Long>> {
         E entity = entityRepository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Resource with id " + id + " not found")
         );
-        Long ownerId = getAuthenticatedUserId();
-        if (!ownerId.equals(getEntityOwnerId(entity))) {
-            throw new UnauthorizedAccessException();
-        }
+        checkOwnership(entity);
 
         return converter.convertToResponse(entity);
     }
@@ -37,10 +35,7 @@ public abstract class BaseService<Q, E, S, R extends JpaRepository<E, Long>> {
         E entity = entityRepository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Resource with id " + id + " not found")
         );
-        Long ownerId = getAuthenticatedUserId();
-        if (!ownerId.equals(getEntityOwnerId(entity))) {
-            throw new UnauthorizedAccessException();
-        }
+        checkOwnership(entity);
 
         entityRepository.deleteById(id);
     }
@@ -49,9 +44,39 @@ public abstract class BaseService<Q, E, S, R extends JpaRepository<E, Long>> {
         return AuthContext.getUserId();
     }
 
-    public abstract S add(Q request);
+    public S add(Q request) {
+        if (request == null) {
+            throw new NullPointerException("Null request dto provided");
+        }
+        E entity = converter.convertToEntity(request);
+        performOnAdd(entity);
+        return converter.convertToResponse(entityRepository.save(entity));
+    }
 
-    public abstract S update(Q request, Long id);
+    public S update(Q request, Long id) {
+        if (id == null || id < 0 || request == null) {
+            throw new IllegalArgumentException();
+        }
+        E entity = entityRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Resource with id" + id + " not found")
+        );
+        checkOwnership(entity);
+        performUpdate(entity, request);
+        return converter.convertToResponse(entityRepository.save(entity));
+    }
+
+    protected void checkOwnership(E entity) {
+        Long ownerId = getAuthenticatedUserId();
+        if (!ownerId.equals(getEntityOwnerId(entity))) {
+            throw new UnauthorizedAccessException();
+        }
+    }
+
+    protected abstract void performUpdate(E entity, Q request);
+
+    protected abstract void performOnAdd(E entity);
 
     protected abstract Long getEntityOwnerId(E entity);
+
+
 }
